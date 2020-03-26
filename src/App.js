5c519a73efcb8da397dc5117e214fe1af2b898e9
Sachin <mittalsam98@gmail.com>
2020-03-26 17:49:30 +0530

    added client and node setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useForm} from 'react-hook-form'
 
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => { console.log(data) }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        A twitter clone to only tweets
       </header>
+      <form className='form' onSubmit={handleSubmit(onSubmit)}>
+        <label for='name'>Name</label><br />
+        <input className='input' name="name" ref={register({required:true})} /><br />
+        <label className='label' for='tweets'>Tweets</label><br />
+      <textarea className='input' name="tweets" ref={register({ required: true })} /><br />
+
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <button className='button' type="submit">Submit</button>
+      
+    </form>
     </div>
   );
 }
